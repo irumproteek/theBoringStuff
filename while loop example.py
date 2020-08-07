09db0ea3f0d5ab1333d98208736c388deee191dd
irumproteek <65669672+irumproteek@users.noreply.github.com>
2020-08-07 13:04:52 -0400

    Add files via upload

---
+++ while loop example.py
@@ -0,0 +1,6 @@
+#annoying while loop example
+name = '' #an empty sting so that "name != 'your name':" evaluates to true
+while name != 'your name':
+    print('Please type your name.')
+    name = input()
+print('Thank you!')
