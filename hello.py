f844d460d8cdf384dd6a8c25a845e87fd37a0b81
irumproteek <65669672+irumproteek@users.noreply.github.com>
2020-08-06 20:03:27 -0400

    Add files via upload
    This is simple program that asks for your name and age.

---
+++ hello.py
@@ -0,0 +1,9 @@
+#this program say hello and asks for your name
+print('hello world')
+print('what is your name?')
+myName = input()
+print("it's good to meet you " + myName)
+print("the length of your name is " + str(len(myName)) + " characters long")
+print("what is your age?")
+myAge = input()
+print("wow! you will be " + str(int(myAge) + 1) + " in a year")
